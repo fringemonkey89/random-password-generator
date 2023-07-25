96f1994dbe42d8407edf4d89f4ef6f3f74766679
fringemonkey89 <markomajeticjr@gmail.com>
2023-07-25 12:58:05 -0400

    added comments to script.js file

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 // Assignment code here
-//variables for password generator
-function generatePassword(){
-  var lowerCase = confirm("Do you want your password to contain lowercase letters?")
-  var upperCase = confirm("Do you want your password to contain uppercase letters?")
-  var num = confirm("Do you want your password to contain any numbers?")
-  var specialCharacter = confirm("Do you want your password to contain special characters?")
-  var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.")
-  
 
-var character = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//this is the function that generates a password based on a series of prompts or questions
+function generatePassword(){
+
+//variables for password generator
+var character = "!@#$%^&*()?.<\>|=+:;,[-_]"
 
 var number = "0123456789";
 
@@ -18,12 +14,50 @@ var upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var enter = [];
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-var randomString = "";
+//these are the criteria a user will choose for his password
+var lowerCase = confirm("Do you want your password to contain lowercase letters?")
+var upperCase = confirm("Do you want your password to contain uppercase letters?")
+var num = confirm("Do you want your password to contain any numbers?")
+var specialCharacter = confirm("Do you want your password to contain special characters?")
+var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.")
+  
+//this function will check if the password length is between 8 and 128 characters
+if (passwordLength < 8 || passwordLength > 128){
+  alert("your password is not bewtween 8 characters or 128 characters. Try again.")
+  var passwordLength = prompt("choose a password that is at least 8 characters but no more than 128 characters")
+}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//this code is for anyone who doesn't select any criteria and asks them to choose one
+if (lowerLetters === false && upperLetters === false && number === false && character === false){
+  alert("you have to choose at least one criteria")
+  var lowerCase = confirm("Do you want your password to contain lowercase letters?")
+  var upperCase = confirm("Do you want your password to contain uppercase letters?")
+  var num = confirm("Do you want your password to contain any numbers?")
+  var specialCharacter = confirm("Do you want your password to contain special characters?")
+}
+
+//using the chosen criteria to generate the password
+if(lowerCase){
+  enter = enter + lowerLetters;
+}
+if(upperCase){
+  enter = enter + upperLetters;
+}
+if(number){
+  enter = enter + number;
+}
+if(specialCharacter){
+  enter = enter + character;
+}
+
+//generates a random password based on selected criteria
+let generatedPassword = ""
+for (let i = 0; i < passwordLength; i++){
+  let random = [Math.floor(Math.random() * enter.length)];
+  generatedPassword = generatedPassword + enter[random];
+}
+return generatedPassword
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -33,43 +67,21 @@ function writePassword() {
 
   passwordText.value = password;
 }
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
  
 
 
 
-if (passwordLength < 8 || passwordLength > 128){
-  alert("your password is not bewtween 8 characters or 128 characters. Try again.")
-  var passwordLength = prompt("choose a password that is at least 8 characters but no more than 128 characters")
-}
 
-else if (lowerCase === false && upperCase === false && num === false && specialCharacter === false){
-  alert("you have to choose at least one criteria")
-  var lowerCase = confirm("Do you want your password to contain lowercase letters?")
-  var upperCase = confirm("Do you want your password to contain uppercase letters?")
-  var num = confirm("Do you want your password to contain any numbers?")
-  var specialCharacter = confirm("Do you want your password to contain special characters?")
-}
 
-if(lowerCase){
-  enter = enter + lowerLetters;
-}
 
-if(upperCase){
-  enter = enter + upperLetters;
-}
 
-if(number){
-  enter = enter + number;
-}
 
-if(specialCharacter){
-  enter = enter + character;
-}
 
-for (var i = 0; i < passwordLength; i++){
-  randomString += enter.charAt(Math.floor(Math.random() * enter.length));
-}
-
-passwordText = randomString;
-
-//Asks for input from user
